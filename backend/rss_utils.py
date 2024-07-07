cb7ff92f24f9890db90dd7522e50a78376d985ce
David Gorski <gorski.dave@gmail.com>
2024-07-07 02:50:05 -0400

    More formatting

--- backend/rss_utils.py
+++ backend/rss_utils.py
@@ -4,4 +4,7 @@ import time
 # Get all URLs from a feed
 def get_all_links(feed_url: str):
     feed = feedparser.parse(feed_url)
-    return [{ 'link': entry.link, 'date': int(time.mktime(entry.published_parsed)) } for entry in feed.entries]
+    return [{
+        'link': entry.link, 'date': int(time.mktime(entry.published_parsed)) }
+        for entry
+        in feed.entries]
