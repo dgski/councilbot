2a0d6b0277ea8599cfe08a726d91de2e2dccfd6d
David Gorski <gorski.dave@gmail.com>
2024-07-07 10:51:58 -0400

    Add front-end

---
+++ frontend/src/routes/+page.svelte
@@ -0,0 +1,77 @@
+<script>
+    import { onMount } from 'svelte';
+
+    /**
+     * @type {any[]}
+     */
+    let meetings = []
+
+    onMount(() => {
+        fetch('https://councilbot-api.xantasoft.com/meetings?city_id=84245ba6-50f4-4ecb-9ebd-918c38a349ef')
+            .then(res => res.json())
+            .then(data => {
+                meetings = data;
+            });
+    });
+
+    let toString = (seconds) => {
+        let date = new Date(seconds * 1000);
+        return date.toDateString();
+    }
+</script>
+
+<style>
+    :global(body) {
+        font-family: Arial, sans-serif;
+        margin: 0;
+    }
+
+    #hero {
+        background-color: #f4f4f4;
+        color: #333;
+        padding: 10px;
+        padding-bottom: 80px;
+        width: 100%;
+        margin: 0 auto;
+        padding-top: 80px;
+    }
+
+    #hero-content {
+        width: 800px;
+        margin-left: calc(50% - 400px);
+        font-size: 1.5em;
+    }
+
+    #hero-question {
+        font-size: 1.5em;
+        font-weight: 800;
+    }
+
+    #main {
+        width: 800px;
+        margin: 0 auto;
+        line-height: 1.5em;
+        margin-top: 2em;
+    }
+</style>
+
+<div id="hero">
+    <div id="hero-content">
+        <p id="hero-question">Do you know what Waterloo City Council is doing with your tax money and city?</p>
+        <p><b>councilbot</b> gives you a seat at the table by using AI to summarize hours of meetings.</p>
+    </div>
+</div>
+<div id="main">
+    <div id="main-content">
+        {#each meetings as meeting}
+            <div class="meeting">
+                <h3><a href="/meeting/{meeting.meeting_id}">{toString(meeting.meeting_date)}</a></h3>
+                <ul>
+                    {#each meeting.meeting_decisions as decision}
+                        <li>{decision}</li>
+                    {/each}
+                </ul>
+            </div>
+        {/each}
+    </div>
+</div>
---
+++ frontend/src/routes/meeting/[id]/+page.svelte
@@ -0,0 +1,96 @@
+<script>
+// @ts-nocheck
+
+    import { page } from '$app/stores';
+    import { onMount } from 'svelte';
+
+    /**
+     * @type {{ meeting_date: any; meeting_keywords: any; meeting_segments: any; meeting_decisions: any; } | null}
+     */
+    let data = null;
+
+    // Get id from URL
+    page.subscribe(value => {
+        fetch(`https://councilbot-api.xantasoft.com/meeting/${value.params.id}`)
+            .then(res => res.json())
+            .then(incoming => {
+                console.log(incoming);
+                data = incoming;
+            });
+    });
+
+    let toString = (seconds) => {
+        let minutes = Math.floor(seconds / 60);
+        let remainingSeconds = seconds % 60;
+        return `${minutes}:${remainingSeconds < 10 ? '0' : ''}${remainingSeconds}`;
+    }
+</script>
+
+<style>
+    :global(body) {
+        font-family: Arial, sans-serif;
+        margin: 0;
+    }
+
+    #header {
+        background-color: #f4f4f4;
+        color: #333;
+        padding: 10px;
+        width: 100%;
+        margin: 0 auto;
+        text-align: center;
+        font-weight: 800;
+    }
+    
+    #main {
+        width: 800px;
+        margin: 0 auto;
+        line-height: 1.5em;
+        padding-top: 50px;
+    }
+
+    h1 {
+        font-size: 2em;
+        margin-bottom: 2em;
+    }
+
+    h3 {
+        font-size: 1.5em;
+        margin-top: 1em;
+    }
+</style>
+
+<div id="header">
+    <a href="/">councilbot (Waterloo)</a> 
+</div>
+<div id="main">
+    {#if data}
+        <h1>{new Date(data.meeting_date * 1000).toDateString()}</h1>
+        <h3>Decisions</h3>
+        <ul>
+            {#each data.meeting_decisions as decision}
+                <li>{decision}</li>
+            {/each}
+        </ul>
+        <h3>Segments</h3>
+        <ul>
+            {#each data.meeting_segments as segment}
+                <li><b>{toString(segment.start_time_seconds)} - {toString(segment.end_time_seconds)}</b>: {segment.text}</li>
+            {/each}
+        </ul>
+        <h3>Keywords</h3>
+        <p>
+            {#each data.meeting_keywords as keyword}
+                {keyword},&nbsp;
+            {/each}
+        </p>
+        <h3>Summary</h3>
+        <p>
+            {data.meeting_summary ? data.meeting_summary : "No summary available"}
+        </p>
+        <h3>Video Link</h3>
+        <a href={data.meeting_link}>{data.meeting_link}</a>
+    {:else}
+        <h1>No ID</h1>
+    {/if}
+</div>
