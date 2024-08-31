bb009368d9a72315594b8cfc3510f298851bb339
David Gorski <gorski.dave@gmail.com>
2024-08-31 15:57:27 -0400

    Multi-city support

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,90 +1,9 @@
+// Redirect to page
 <script>
+    import { goto } from '$app/navigation';
     import { onMount } from 'svelte';
-    import Meeting from '../Meeting.svelte';
-
-    /**
-     * @type {any[]}
-     */
-    let meetings = []
 
     onMount(() => {
-        fetch('https://councilbot-api.xantasoft.com/meetings?city_id=84245ba6-50f4-4ecb-9ebd-918c38a349ef')
-            .then(res => res.json())
-            .then(data => {
-                meetings = data;
-            });
+        goto('/city/84245ba6-50f4-4ecb-9ebd-918c38a349ef');
     });
 </script>
-
-<style>
-    :global(body) {
-        font-family: Arial, sans-serif;
-        margin: 0;
-    }
-
-    #hero {
-        background-color: #f4f4f4;
-        color: #333;
-        padding: 10px;
-        padding-bottom: 80px;
-        width: 100%;
-        margin: 0 auto;
-        padding-top: 80px;
-    }
-
-    #hero-content {
-        width: 800px;
-        margin-left: calc(50% - 400px);
-        font-size: 1.5em;
-    }
-
-    #hero-question {
-        font-size: 1.5em;
-        font-weight: 800;
-    }
-
-    #main {
-        width: 800px;
-        margin: 0 auto;
-        line-height: 1.5em;
-        margin-top: 2em;
-    }
-
-    @media (max-width: 800px) {
-        #hero {
-            padding: 0px;
-            padding-top: 20px;
-            padding-bottom: 20px;
-        }
-        #hero-content {
-            width: calc(100% - 40px);
-            margin-left: 0;
-            padding-left: 20px;
-            padding-right: 20px;
-            font-size: 1em;
-        }
-
-        #main {
-            width: calc(100% - 40px);
-            padding: 20px;
-            font-size: 1em;
-            margin-top: 0;
-        }
-    }
-</style>
-<div id="nav">
-
-</div>
-<div id="hero">
-    <div id="hero-content">
-        <p id="hero-question">Do you know what the Waterloo City Council is doing with your tax money and city?</p>
-        <p><b>councilbot</b> gives you a seat at the table by using AI to summarize hours of meetings.</p>
-    </div>
-</div>
-<div id="main">
-    <div id="main-content">
-        {#each meetings as meeting}
-            <Meeting {meeting} />
-        {/each}
-    </div>
-</div>
